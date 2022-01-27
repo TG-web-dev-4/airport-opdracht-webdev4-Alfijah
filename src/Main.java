f7815e495a4734d719a2cd19e33d578e77f15f8c
Alfijah <ajsarban@gmail.com>
2022-01-27 09:50:42 +0100

    Base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner airportsData = new Scanner(new File("C:\\TechGrounds\\TG Web Development\\Java\\airport-opdracht-webdev4-Alfijah\\resources\\airports.csv"));
+        airportsData.useDelimiter(",");
+        while (airportsData.hasNext()) {
+            System.out.println(airportsData.next());
+        }
+        airportsData.close();
     }
 }
